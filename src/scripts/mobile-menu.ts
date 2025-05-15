ebf7533cb0900af95523e0421516690111b5cfa7
matthew-millard <123032297+matthew-millard@users.noreply.github.com>
2025-05-15 15:07:30 -0400

    refactor mobile menu functionality to improve accessibility and user experience; encapsulate open and close menu logic in separate functions, and add event listeners for overlay and keyboard interactions

--- src/scripts/mobile-menu.ts
+++ src/scripts/mobile-menu.ts
@@ -4,16 +4,28 @@ const mobileMenu = document.querySelector('#mobile-menu') as HTMLDivElement;
 const overlay = document.querySelector('#mobile-menu-overlay') as HTMLDivElement;
 const pageContainer = document.querySelector('#page-container') as HTMLDivElement;
 
-openMobileMenuButton?.addEventListener('click', () => {
-  mobileMenu.classList.add('expanded');
-  pageContainer.setAttribute('inert', '');
-  overlay.setAttribute('aria-hidden', 'false');
-  closeMobileMenuButton.focus();
-});
-
-closeMobileMenuButton?.addEventListener('click', () => {
+function closeMobileMenu() {
   mobileMenu.classList.remove('expanded');
   pageContainer.removeAttribute('inert');
   overlay.setAttribute('aria-hidden', 'true');
   openMobileMenuButton.focus();
+}
+
+function openMobileMenu() {
+  mobileMenu.classList.add('expanded');
+  pageContainer.setAttribute('inert', '');
+  overlay.setAttribute('aria-hidden', 'false');
+  closeMobileMenuButton.focus();
+}
+
+openMobileMenuButton.addEventListener('click', openMobileMenu);
+
+closeMobileMenuButton.addEventListener('click', closeMobileMenu);
+
+overlay.addEventListener('click', closeMobileMenu);
+
+mobileMenu.addEventListener('keydown', event => {
+  if (event.key == 'Escape') {
+    closeMobileMenu();
+  }
 });

f92694dee4fd84bbe1138844418969cb83a46ce0
matthew-millard <123032297+matthew-millard@users.noreply.github.com>
2025-05-15 13:57:48 -0400

    refactor CloseButton styles for improved aesthetics; update index page structure to include a page container, and enhance mobile menu functionality with inert attribute management

--- src/scripts/mobile-menu.ts
+++ src/scripts/mobile-menu.ts
@@ -1,11 +1,24 @@
 const toggleMobileMenuButton = document.querySelector('#toggle-mobile-menu-button') as HTMLButtonElement;
 const closeMobileMenuButton = document.querySelector('#close-mobile-menu-button') as HTMLButtonElement;
+const pageContainer = document.querySelector('#page-container') as HTMLDivElement;
 const mobileMenu = document.querySelector('#mobile-menu') as HTMLDivElement;
 
 toggleMobileMenuButton?.addEventListener('click', () => {
-  mobileMenu.classList.toggle('expanded');
+  const isMenuOpen = mobileMenu.classList.contains('expanded');
+
+  if (isMenuOpen) {
+    mobileMenu.classList.remove('expanded');
+    pageContainer.removeAttribute('inert');
+    toggleMobileMenuButton.focus();
+  } else {
+    mobileMenu.classList.add('expanded');
+    pageContainer.setAttribute('inert', 'true');
+    closeMobileMenuButton.focus();
+  }
 });
 
 closeMobileMenuButton?.addEventListener('click', () => {
   mobileMenu.classList.remove('expanded');
+  pageContainer.removeAttribute('inert');
+  toggleMobileMenuButton.focus();
 });

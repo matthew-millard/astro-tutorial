cc71da8bc1d1c2001ed7749f19449c1f8a8db908
matthew-millard <123032297+matthew-millard@users.noreply.github.com>
2025-05-22 12:25:54 -0400

    Refactor layout structure by removing NewLayout and updating index and about pages to use BaseLayout. Adjust FooterLayout styles for consistency and enhance mobile menu script by commenting out unused page container references.

--- src/scripts/mobile-menu.ts
+++ src/scripts/mobile-menu.ts
@@ -2,14 +2,14 @@ const openMobileMenuButton = document.querySelector('#open-mobile-menu-button') 
 const closeMobileMenuButton = document.querySelector('#close-mobile-menu-button') as HTMLButtonElement;
 const mobileMenu = document.querySelector('#mobile-menu') as HTMLDivElement;
 const overlay = document.querySelector('#mobile-menu-overlay') as HTMLDivElement;
-const pageContainer = document.querySelector('.page-container') as HTMLDivElement;
+// const pageContainer = document.querySelector('.page-container') as HTMLDivElement;
 
 // For the prevent scroll and returning to the last scroll position after mobile menu is closed,
 // I referred to this blog post by Brad Wu for inspiration: https://css-tricks.com/prevent-page-scrolling-when-a-modal-is-open/
 function closeMobileMenu() {
   const lastScrollPosition = Number(document.body.getAttribute('data-scroll-y'));
   mobileMenu?.classList.remove('expanded');
-  pageContainer?.removeAttribute('inert');
+  // pageContainer?.removeAttribute('inert');
   overlay?.setAttribute('aria-hidden', 'true');
   document.body.classList.remove('prevent-scroll');
   window.scrollTo(0, lastScrollPosition);
@@ -22,7 +22,7 @@ function openMobileMenu() {
   document.body.setAttribute('data-scroll-y', scrollY.toString());
   document.body.classList.add('prevent-scroll');
   mobileMenu?.classList.add('expanded');
-  pageContainer?.setAttribute('inert', '');
+  // pageContainer?.setAttribute('inert', '');
   overlay?.setAttribute('aria-hidden', 'false');
   closeMobileMenuButton?.focus();
   document.body.scrollTo({ top: scrollY });

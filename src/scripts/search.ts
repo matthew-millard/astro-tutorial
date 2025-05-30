68bbb6b11977589d41643d2923d4f206bb742540
matthew-millard <123032297+matthew-millard@users.noreply.github.com>
2025-05-30 13:25:27 -0400

    Add no-results message to article search; update search logic to display message when no articles match the query.

--- src/scripts/search.ts
+++ src/scripts/search.ts
@@ -1,6 +1,7 @@
 const input = document.querySelector('#search-input') as HTMLInputElement;
 const ul = document.querySelector('#list-of-articles') as HTMLUListElement;
 const a = ul.querySelectorAll('a') as NodeListOf<HTMLAnchorElement>;
+const noResultsMessage = ul.querySelector('.no-results-message') as HTMLParagraphElement;
 
 function moveFocusToSearch(e: KeyboardEvent) {
   if (e.metaKey && e.key === 'k') {
@@ -10,15 +11,26 @@ function moveFocusToSearch(e: KeyboardEvent) {
 
 function handleSearch() {
   const query = input.value.toUpperCase();
+  let searchResults = 0;
+  console.log(noResultsMessage.classList.contains('show-message'));
+
+  if (noResultsMessage.classList.contains('show-message')) {
+    noResultsMessage.classList.toggle('show-message');
+  }
 
   for (let i = 0; i < a.length; i++) {
     let p = a[i].querySelectorAll('p')[0] as HTMLParagraphElement;
 
     if (p.innerHTML.toUpperCase().indexOf(query) > -1) {
       a[i].style.display = '';
+      searchResults++;
     } else {
       a[i].style.display = 'none';
     }
+  }
+
+  if (!searchResults) {
+    noResultsMessage.classList.add('show-message');
   }
 }
 
